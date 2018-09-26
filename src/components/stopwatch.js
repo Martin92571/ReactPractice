5881495a7d88b9bff422bf4a3915c444530a8b7a
Martin92571 <zegrizo@gmail.com>
2018-09-25 20:54:00 -0700

    added a header and h1 title to react component practice

---
+++ src/components/stopwatch.js
@@ -0,0 +1,28 @@
+import React,{ Component } from 'react';
+
+class Stopwatch extends Component{
+    constructor(props){
+        super(props);
+         this.state={
+            status:'stopped',
+            start:null,
+            elapsed:0
+        };
+    }
+    start(){
+       this.setState{}
+    }
+    stop(){
+
+    }
+    render(){
+        const {elapsed,status}=this.state;
+        return( <div>
+              <h1>{elapsed}</h1>
+              <p>{status}</p>
+              </div>)
+    
+    }
+     
+}
+export default Stopwatch;
