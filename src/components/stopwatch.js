1841c7a676d672f96ee9f95da8797fbaf86936be
Martin92571 <zegrizo@gmail.com>
2018-09-25 23:54:07 -0700

    added clock format

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -1,25 +1,59 @@
 import React,{ Component } from 'react';
-
+import { setTimeout } from 'timers';
+import Time from './format_time';
 class Stopwatch extends Component{
     constructor(props){
+        
         super(props);
          this.state={
             status:'stopped',
             start:null,
             elapsed:0
         };
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.update = this.update.bind(this);
+        this.reset = this.reset.bind(this);
     }
     start(){
-       this.setState{}
+       this.setState({
+        status:'running',
+        start:new Date().getTime()
+       });
+       setTimeout(this.update,10);
     }
     stop(){
-
+       
+        this.setState({
+            status:'stop',
+            
+           });
+    }
+    update(){
+        const {status,start}=this.state;
+        if(status==='running'){
+            this.setState({
+                elapsed:new Date().getTime()-start
+            });
+            setTimeout(this.update,10);  
+        }
+        
+    }
+    reset(){
+        this.setState({
+         status:'stopped',
+         start:null,
+         elapsed:0
+        });
     }
     render(){
         const {elapsed,status}=this.state;
         return( <div>
-              <h1>{elapsed}</h1>
+              <h1><Time elapsed={elapsed}/></h1>
               <p>{status}</p>
+              <button onClick={this.start}>Start</button>
+              <button onClick={this.stop}>Stop</button>
+              <button onClick={this.reset}>Reset</button>
               </div>)
     
     }
